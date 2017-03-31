c0e38974c95f5b01ce03ea2a4b6727ea39139b11
Chris Langna <chris.langan195@gmail.com>
2017-03-30 20:48:02 -0600

    Started work on task1

--- gavin_brimhall_chris_langan_task1_hw7.py
+++ gavin_brimhall_chris_langan_task1_hw7.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 import sys
+
+def CheckDoors(args):
+	"""
+	Takes care of all the logic
+	"""
+	LD = 0 #Left dashboard switch
+	RD = 0 #Right dashboard switch
+	CL = 0 #Child Lock
+	ML = 0 #Master unlock switch
+	LI = 0 #Left inside handle
+	LO = 0 #Left outside handle
+	RI = 0 #Right inside handle
+	RO = 0 #Right outside handle
+	GS = 'P' #Gear shift
+	print 'Arg list: ', str(sys.argv)
