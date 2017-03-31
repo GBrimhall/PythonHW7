c913048dbdbde8664d167618669135d50acc9922
Chris Langna <chris.langan195@gmail.com>
2017-03-31 14:45:13 -0600

    Task 1 module 1 functional

--- gavin_brimhall_chris_langan_task1_hw7.py
+++ gavin_brimhall_chris_langan_task1_hw7.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-def CheckDoors(args):
+def CheckDoors(junk, LDa, RDa, CLa, MLa, LIa, LOa, RIa, ROa, GSa):
 	"""
 	Takes care of all the logic
 	"""
@@ -13,5 +13,87 @@ def CheckDoors(args):
 	LO = 0 #Left outside handle
 	RI = 0 #Right inside handle
 	RO = 0 #Right outside handle
-	GS = 'P' #Gear shift
-	print 'Arg list: ', str(sys.argv)
+	GS = 'Z' #Gear shift
+
+	LD = LDa
+	RD = RDa
+	CL = CLa
+	ML = MLa
+	LI = LIa
+	LO = LOa
+	RI = RIa
+	RO = ROa
+	GS = GSa
+
+	if GS == 'P':
+		#Car is in park
+		if ML == 1:
+			#Master lock is active
+			if CL == 1:
+				#Child lock is active, ignore inside door handles
+				#Check left door first
+				if LI == 1:
+					#left inside handle pulled
+					print ("Left door does not open")
+				elif LD == 1:
+					#left dashboard switch pressed
+					print ("Left door opens")
+				elif LO == 1:
+					#left outside handle pulled
+					print ("Left door opens")
+				else:
+					#Nothing is trying to open the left door
+					print ("Left door does not open")
+
+				#Check right door
+				if RI == 1:
+					#right inside door handle pulled
+					print ("Right door does not open")
+				elif RD == 1:
+					#Right dashboard switch pressed
+					print ("Right door opens")
+				elif RO == 1:
+					#Right outside handle pulled
+					print ("Right door opens")
+				else:
+					#Nothing is trying to open the right door
+					print ("Right door does not open")
+			else:
+				#Child lock is not active
+				#Check left door first
+				if LI == 1:
+					#left inside handle pulled
+					print ("Left door opens")
+				elif LD == 1:
+					#left dashboard switch pressed
+					print ("Left door opens")
+				elif LO == 1:
+					#left outside handle pulled
+					print ("Left door opens")
+				else:
+					#Nothing is trying to open the left door
+					print ("Left door does not open")
+
+				#Check right door
+				if RI == 1:
+					#right inside door handle pulled
+					print ("Right door does opens")
+				elif RD == 1:
+					#Right dashboard switch pressed
+					print ("Right door opens")
+				elif RO == 1:
+					#Right outside handle pulled
+					print ("Right door opens")
+				else:
+					#Nothing is trying to open the right door
+					print ("Right door does not open")
+
+		else:
+			#Master lock is not active
+			print("Doors do not open")
+	else:
+		#Car is not in park
+		print ("Doors do not open")
+
+if __name__ == '__main__':
+	CheckDoors(sys.argv[1:])
---
+++ gavin_brimhall_chris_langan_task1_hw7Mod2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import gavin_brimhall_chris_langan_task1_hw7
+
+def main():
+	"""
+	Main function, controls the other module
+	"""
+	gavin_brimhall_chris_langan_task1_hw7.CheckDoors(0, 0, 0, 0, 1, 1, 0, 0, 0, 'P')	
+
+if __name__== '__main__':
+	main()
+	exit(0)
