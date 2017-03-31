41f6ba68ec2508e74c495dbe85719ee6061329dd
Chris Langna <chris.langan195@gmail.com>
2017-03-31 16:38:32 -0600

    Task 1 finished

--- gavin_brimhall_chris_langan_task1_hw7.py
+++ gavin_brimhall_chris_langan_task1_hw7.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-def CheckDoors(junk, LDa, RDa, CLa, MLa, LIa, LOa, RIa, ROa, GSa):
+def CheckDoors(inputList = []):
 	"""
 	Takes care of all the logic
 	"""
@@ -13,87 +13,91 @@ def CheckDoors(junk, LDa, RDa, CLa, MLa, LIa, LOa, RIa, ROa, GSa):
 	LO = 0 #Left outside handle
 	RI = 0 #Right inside handle
 	RO = 0 #Right outside handle
-	GS = 'Z' #Gear shift
+	GS = 'P' #Gear shift
 
-	LD = LDa
-	RD = RDa
-	CL = CLa
-	ML = MLa
-	LI = LIa
-	LO = LOa
-	RI = RIa
-	RO = ROa
-	GS = GSa
+	print (inputList)
+	LD = int(inputList[0])
+	RD = int(inputList[1])
+	CL = int(inputList[2])
+	ML = int(inputList[3])
+	LI = int(inputList[4])
+	LO = int(inputList[5])
+	RI = int(inputList[6])
+	RO = int(inputList[7])
+	GS = inputList[8]
 
-	if GS == 'P':
-		#Car is in park
-		if ML == 1:
-			#Master lock is active
-			if CL == 1:
-				#Child lock is active, ignore inside door handles
-				#Check left door first
-				if LI == 1:
-					#left inside handle pulled
-					print ("Left door does not open")
-				elif LD == 1:
-					#left dashboard switch pressed
-					print ("Left door opens")
-				elif LO == 1:
-					#left outside handle pulled
-					print ("Left door opens")
-				else:
-					#Nothing is trying to open the left door
-					print ("Left door does not open")
-
-				#Check right door
-				if RI == 1:
-					#right inside door handle pulled
-					print ("Right door does not open")
-				elif RD == 1:
-					#Right dashboard switch pressed
-					print ("Right door opens")
-				elif RO == 1:
-					#Right outside handle pulled
-					print ("Right door opens")
-				else:
-					#Nothing is trying to open the right door
-					print ("Right door does not open")
-			else:
-				#Child lock is not active
-				#Check left door first
-				if LI == 1:
-					#left inside handle pulled
-					print ("Left door opens")
-				elif LD == 1:
-					#left dashboard switch pressed
-					print ("Left door opens")
-				elif LO == 1:
-					#left outside handle pulled
-					print ("Left door opens")
-				else:
-					#Nothing is trying to open the left door
-					print ("Left door does not open")
-
-				#Check right door
-				if RI == 1:
-					#right inside door handle pulled
-					print ("Right door does opens")
-				elif RD == 1:
-					#Right dashboard switch pressed
-					print ("Right door opens")
-				elif RO == 1:
-					#Right outside handle pulled
-					print ("Right door opens")
-				else:
-					#Nothing is trying to open the right door
-					print ("Right door does not open")
-
-		else:
-			#Master lock is not active
-			print("Doors do not open")
+	if ((GS != 'P') and (GS != 'N') and (GS != 'D') and (GS != 'R') and (GS != 1) and (GS != 2) and (GS != 3)):
+		print ('Invalid record, both doors stay closed')
 	else:
-		#Car is not in park
-		print ("Doors do not open")
+		if GS == 'P':
+			#Car is in park
+			if ML == 1:
+				#Master lock is active
+				if CL == 1:
+					#Child lock is active, ignore inside door handles
+					#Check left door first
+					if LI == 1:
+						#left inside handle pulled
+						print ("Left door does not open")
+					elif LD == 1:
+						#left dashboard switch pressed
+						print ("Left door opens")
+					elif LO == 1:
+						#left outside handle pulled
+						print ("Left door opens")
+					else:
+						#Nothing is trying to open the left door
+						print ("Left door does not open")
+
+					#Check right door
+					if RI == 1:
+						#right inside door handle pulled
+						print ("Right door does not open")
+					elif RD == 1:
+						#Right dashboard switch pressed
+						print ("Right door opens")
+					elif RO == 1:
+						#Right outside handle pulled
+						print ("Right door opens")
+					else:
+						#Nothing is trying to open the right door
+						print ("Right door does not open")
+				else:
+					#Child lock is not active
+					#Check left door first
+					if LI == 1:
+						#left inside handle pulled
+						print ("Left door opens")
+					elif LD == 1:
+						#left dashboard switch pressed
+						print ("Left door opens")
+					elif LO == 1:
+						#left outside handle pulled
+						print ("Left door opens")
+					else:
+						#Nothing is trying to open the left door
+						print ("Left door does not open")
+
+					#Check right door
+					if RI == 1:
+						#right inside door handle pulled
+						print ("Right door does opens")
+					elif RD == 1:
+						#Right dashboard switch pressed
+						print ("Right door opens")
+					elif RO == 1:
+						#Right outside handle pulled
+						print ("Right door opens")
+					else:
+						#Nothing is trying to open the right door
+						print ("Right door does not open")
+
+			else:
+				#Master lock is not active
+				print("Boths doors stay closed")
+		else:
+			#Car is not in park
+			print ("Both door stay closed")
 
 if __name__ == '__main__':
 	CheckDoors(sys.argv[1:])
--- gavin_brimhall_chris_langan_task1_hw7Mod2.py
+++ gavin_brimhall_chris_langan_task1_hw7Mod2.py
@@ -4,9 +4,30 @@ import gavin_brimhall_chris_langan_task1_hw7
 
 def main():
 	"""
-	Main function, controls the other module
+	Main function, controls the other module, cleans input
 	"""
-	gavin_brimhall_chris_langan_task1_hw7.CheckDoors(0, 0, 0, 0, 1, 1, 0, 0, 0, 'P')	
+
+	#COMMENT FOR PROFESSOR/GRADER
+	#At the time of writing this program, the required csv file, minivanTest.csv
+	#is inaccessable. When attempting to get to the file via the url provided in the
+	#homework description the webpage 404's. Because of this, the program runs off of
+	#a local copy of the file, which is included
+
+	i = 1
+	with open('minivanTest.csv','r') as minivanFile:
+		for line in minivanFile:
+			print ('Reading record ', i)
+			lineWords = line.split()
+			lineWords = [w.replace(',', '') for w in lineWords]
+			if lineWords[0] == 'H1':
+				print ('Header found, skipping')
+				del lineWords
+			elif lineWords[0] == 'R1':
+				del lineWords[0]
+				gavin_brimhall_chris_langan_task1_hw7.CheckDoors(lineWords)	
+			else:
+				print("something is weird")
+			i += 1
 
 if __name__== '__main__':
 	main()
