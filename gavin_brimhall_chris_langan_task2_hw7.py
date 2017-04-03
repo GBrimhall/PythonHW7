ab573340905a333eb8ae2ce8bacdba176f6cdf38
Gavin Brimhall <gavinbrimhall@mail.weber.edu>
2017-04-03 12:23:29 -0600

    final commit of this assignment

--- gavin_brimhall_chris_langan_task2_hw7.py
+++ gavin_brimhall_chris_langan_task2_hw7.py
@@ -2,11 +2,34 @@
 import sys
 import re
 
+
 def printDigit(d):
     """
     Function that prints the digit to bar code
     """
 
+    #Print the bar code according to the number in the zip code
+    for j in d:
+        if d[j] == 0:
+            print("||:::")
+        elif d[j] == 1:
+            print(":::||")
+        elif d[j] == 2:
+            print("::|:|")
+        elif d[j] == 3:
+            print("::||:")
+        elif d[j] == 4:
+            print(":|::|") 
+        elif d[j] == 5:
+            print(":|:|:")
+        elif d[j] == 6:
+            print(":||::")
+        elif d[j] == 7:
+            print("|:::|")
+        elif d[j] == 8:
+            print("|::|:")
+        elif d[j] == 9:
+            print("|:|::")
 
 
 def printBarCode(zipCode):
@@ -15,28 +38,24 @@ def printBarCode(zipCode):
     calls printDigit to print the zip code numbers and the checkDigit
     """
     
+    #add all the digits
     for i in zipCode:
         checkDigit = (10 - sum(zipCode[i])%10)
 
 
-    #Zip code validation
+    #Zip code validation and print
     if (re.match("^[0-9]*$", zipCode): 
         if(Len(zipCode) == 5):
             print("|")
             printDigit(zipCode)
             print(checkDigit + "|" )
             else:
-                print("Zip is not all numeric")
+                print("Zip code is not 5 digits")
     else:
         print("Zip code is not all numeric")
 
 
-
-
-
-
-
-
+        
 #Main function
 def main():
     """
